90507fbcc3341c63ac9d572ed4cbd69500b0184e
Night <72084642+nightchao@users.noreply.github.com>
2022-02-14 17:39:52 +0700

    Update ConvertNumberTest.java

--- FormatNumber/ConvertNumberTest.java
+++ FormatNumber/ConvertNumberTest.java
@@ -6,67 +6,67 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import dev.run.ConvertNumber;
+import dev.ConvertNumber;
 
 public class ConvertNumberTest {
 
 	private ConvertNumber a;
-	
+
 	@Before
 	public void setup() {
 		a = new ConvertNumber();
 	}
-	
+
 	@Test
 	public void test1() {
-		assertEquals("999", a.convertNumer(999L));
+		assertEquals("999", a.formatNumber(999L));
 	}
-	
+
 	@Test
 	public void test2() {
-		assertEquals("1.00k", a.convertNumer(1000L));
+		assertEquals("1K", a.formatNumber(1000L));
 	}
-	
+
 	@Test
 	public void test3() {
-		assertEquals("1.46k", a.convertNumer(1459L));
+		assertEquals("1.46K", a.formatNumber(1459L));
 	}
-	
+
 	@Test
 	public void test33() {
-		assertEquals("10.0k", a.convertNumer(9999L));
+		assertEquals("10K", a.formatNumber(9999L));
 	}
-	
+
 	@Test
 	public void test4() {
-		assertEquals("17.5k", a.convertNumer(17541L));
+		assertEquals("17.5K", a.formatNumber(17541L));
 	}
-	
+
 	@Test
 	public void test5() {
-		assertEquals("100k", a.convertNumer(100000L));
+		assertEquals("100K", a.formatNumber(100000L));
 	}
-	
+
 	@Test
 	public void test6() {
-		assertEquals("123k", a.convertNumer(123400L));
+		assertEquals("123K", a.formatNumber(123400L));
 	}
-	
+
 	@Test
 	public void test7() {
-		assertEquals("1.00m", a.convertNumer(1000000L));
+		assertEquals("1M", a.formatNumber(1000000L));
 	}
-	
+
 	@Test
 	public void test8() {
-		assertEquals("100m", a.convertNumer(100000000L));
+		assertEquals("100M", a.formatNumber(100000000L));
 	}
-	
+
 	@Test
 	public void test9() {
-		assertEquals("12.3m", a.convertNumer(12345678L));
+		assertEquals("12.3M", a.formatNumber(12345678L));
 	}
-	
+
 	@After
 	public void tearDown() {
 		a = null;
